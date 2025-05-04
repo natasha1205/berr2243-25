c9c924f550bd5f70fc879363c2a3ea0c02a24275
natasha <nurnadianatasha03@gmail.com>
2025-05-04 23:01:21 +0800

    exercise 2

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ async function main(){
     const uri = "mongodb://localhost:27017"
     const client = new MongoClient(uri);
 
+
+    const drivers = [
+      {name: "john Doe", vehicleType:"sedan",isAvailable: true , rating: 4.8},
+      {name: "Alice Smith", vehicleType:"SUV",isAvailable: false , rating: 4.5},
+      {name: "Haris j",vehicleType:"honda",isAvailable: true , rating: 4.7}
+    ];
+
+    //show the data in the console
+    console.log(drivers);
+    
 try{
     await client.connect();
     console.log("Connected to MongoDB!");
